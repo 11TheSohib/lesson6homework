c9dc916ca9f44515c1ad9e297762c16c1f05d915
11TheSohib <sohibjonusmonjonov1@gmail.com>
2025-03-25 08:16:04 +0500

    lesson6Homework
    lesson6Homework

---
+++ app.js
@@ -0,0 +1,10 @@
+// const matn = "  salom dunyoda nima gaplar  "
+
+// function kopMas(matn, raqam){
+//     const nuqta = '...'
+//     matn = matn.trim()
+//     matn = matn.slice(0, raqam) + nuqta
+//     return matn
+// }
+
+// console.log(kopMas(matn, 10));
